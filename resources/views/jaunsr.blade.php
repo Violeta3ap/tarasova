bf7f0a97423aedb94768887e95958505f7d434fa
Violeta <violetatarasova35@gmail.com>
2025-10-23 08:02:06 +0000

    rez new

--- resources/views/jaunsr.blade.php
+++ resources/views/jaunsr.blade.php
@@ -8,16 +8,45 @@
 <form method="POST" action="/data/rezervacijas/jaunsSubmit">
     @csrf
     <div class="mb-3">
-        <label for="Vards" class="form-label">Vārds</label>
-        <input type="text" class="form-control" id="Vards" name="Vards" required>
+        <label for="KlientaID" class="form-label">Klienta ID</label>
+        <input type="text" class="form-control" id="KlientaID" name="KlientaID" required>
     </div>
  
     <div class="mb-3">
-        <label for="Uzvards" class="form-label">Uzvārds</label>
-        <input type="text" class="form-control" id="Uzvards" name="Uzvards">
+        <label for="NumuraID" class="form-label">Numura ID</label>
+        <input type="text" class="form-control" id="NumuraID" name="NumuraID">
     </div>
 
  
+    
+    <div class="mb-3">
+        <label for="IebrauksanasDatums" class="form-label">Iebrauksanas datums</label>
+        <input type="text" class="form-control" id="IebrauksanasDatums" name="IebrauksanasDatums">
+    </div>
+
+
+     
+    <div class="mb-3">
+        <label for="IzbrauksanasDatums" class="form-label">Izbrauksanas datums</label>
+        <input type="text" class="form-control" id="IzbrauksanasDatums" name="IzbrauksanasDatums">
+    </div>
+
+
+    <div class="mb-3">
+        <label for="RezervacijasStatuss" class="form-label">Rezervacijas statuss</label>
+        <input type="text" class="form-control" id="RezervacijasStatuss" name="RezervacijasStatuss">
+    </div>
+
+
+     
+    <div class="mb-3">
+        <label for="DarbiniekaID" class="form-label">Darbinieka ID</label>
+        <input type="text" class="form-control" id="DarbiniekaID" name="DarbiniekaID">
+    </div>
+
+
+
     <button type="submit" class="btn btn-success">Saglabāt</button>
 </form>
 @endsection
+
